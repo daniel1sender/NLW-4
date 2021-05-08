15ddae9e8d95e6289c14a1f0e8efc00dba0daf39
daniel sender <daniel1sender>
2021-05-07 21:08:21 -0300

    first commit

---
+++ src/components/Profile.tsx
@@ -0,0 +1,20 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../Contexts/ChallengesContext';
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+    const { level } = useContext(ChallengesContext);
+
+    return (
+        <div className={styles.profileContainer}>
+            <img src="https://github.com/daniel1sender.png" alt="Daniel Sender" />
+            <div>
+                <strong>Daniel Sender</strong>
+                <p>
+                    <img src="icons/level.svg" alt="Level" />
+                    Level {level}
+                </p>
+            </div>
+        </div>
+    );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,37 @@
+import Head from 'next/head';
+
+import { CompletedChallenges } from '../components/CompletedChallenges';
+import { Countdown } from '../components/Countdown';
+import { ExperienceBar } from '../components/ExperienceBar';
+import { Profile } from '../components/Profile';
+import { ChallengeBox } from '../components/ChallengeBox';
+
+
+import styles from '../styles/pages/Home.module.css';
+import { CountdownProvider } from '../Contexts/CountDownContext';
+
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <head>
+        <title>Início | move.it</title>
+      </head>
+
+      <ExperienceBar />
+
+      <CountdownProvider>
+        <section>
+          <div>
+            <Profile />
+            <CompletedChallenges />
+            <Countdown />
+          </div>
+          <div>
+            <ChallengeBox />
+          </div>
+        </section>
+      </CountdownProvider>
+    </div>
+  )
+}
